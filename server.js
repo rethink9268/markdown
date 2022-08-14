ea4f5cbcb68be69caaaac4d75cf6bd46127b99a4
rethink9268 <pvthuan92@gmail.com>
2022-08-14 23:20:20 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose"); //database//
+const articleRouter = require("./routes/articles.js");
+const app = express();
+const Article = require("./models/articles.js");
+const methodOverride = require("method-override");
+
+//connect database//
+mongoose.connect("mongodb://localhost/blog");
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(methodOverride("_method"));
+
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({
+    createdAt: "desc",
+  });
+  res.render("articles/index", { articles: articles });
+});
+
+app.use("/articles", articleRouter);
+
+app.listen(5000);
